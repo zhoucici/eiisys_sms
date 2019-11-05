4172bd7159ad7518c1d972fc4a1d887fe11307df
zch_gddr <zch_gddr@163.com>
2019-11-05 13:35:01 +0800

    111

--- src/assets/config/address-config.js
+++ src/assets/config/address-config.js
@@ -9,15 +9,18 @@ const keyObj = {
 const webUrl = {
     // 开发模式
     dev:{
-        eiisys_sms:'/eiisys_sms'
+        eiisys_sms:'/eiisys_sms',
+        eiisys_smsindex:"https://peiisys.eiisys.com/html/eiisys-index.html",  // 海翕云对应官网
     },
     // 测试模式
     test:{
-        eiisys_sms:'http://122.112.204.156:8290'
+        eiisys_sms:'http://122.112.204.156:8290',
+        eiisys_smsindex:"https://peiisys.eiisys.com/html/eiisys-index.html",  // 海翕云对应官网
     },
     // 线上模式
     online:{
-        eiisys_sms:'http://122.112.204.156:8290'
+        eiisys_sms:'http://122.112.204.156:8290',
+        eiisys_smsindex:"eiisys.com",  // 海翕云对应官网
     },
 }
 
